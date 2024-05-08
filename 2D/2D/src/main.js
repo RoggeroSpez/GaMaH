7b004a6fe1fde3fae5847a2d48f4e19a5db7f7f3
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2024-05-08 13:45:10 -0700

    updates

--- 2D/2D/src/main.js
+++ 2D/2D/src/main.js
@@ -3,7 +3,9 @@ import {k} from "kaboomCtx";
 k.loadSprite("spritesheet", "./spritesheet.png", {
     sliceX: 39,
     sliceY: 31,
-
+    anims: {
+        "idle-down": 936,
+    }
 });
 
 //frames are 16x16 tile
